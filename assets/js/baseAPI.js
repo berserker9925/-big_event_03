a4f74c8907987a5578f40e3cd0eb00396614d353
Berserker <3311435980@qq.com>
2021-03-15 10:10:28 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,31 @@
-let baseURL = "http://ajax.frontend.itheima.net"
-$.ajaxPrefilter(function (params) {
-    params.url = baseURL + params.url;
-})
+let baseURL = "http://ajax.frontend.itheima.net";
+$.ajaxPrefilter(function (options) {
+  options.url = baseURL + options.url;
+
+  if (options.url.indexOf("/my/") !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem("token") || "",
+    };
+
+    options.complete = function (res) {
+      let obj = res.responseJSON;
+      if (obj.status == 1 && obj.message === "身份认证失败！") {
+        localStorage.removeItem("token");
+        location.href = "./login.html";
+      }
+    };
+  }
+
+  // 拦截所有响应，判断身份认证信息
+  // params.complete = function (res) {
+  // console.log(res.responseJSON);
+  // // 判断  如果状态码是1  错误信息是身份认证失败，那么就跳转到登录页面
+  // let obj = res.responseJSON;
+  // if (obj.status == 1 && obj.message === "身份认证失败！") {
+  //   // 清空本地token
+  //   localStorage.removeItem("token");
+  //   // 页面跳转
+  //   location.href = "./login.html";
+  // }
+  // },
+});
---
+++ assets/js/index.js
@@ -0,0 +1,58 @@
+$(function () {
+  getUserInof();
+
+  // 退出登录
+  $("#btnlogout").on("click", function () {
+    //   弹出退出框，清空token，跳转登录页面
+    layer.confirm(
+      "是否确定退出?",
+      { icon: 3, title: "提示" },
+      function (index) {
+        //do something
+        localStorage.removeItem("token");
+        location.href = "./login.html";
+        layer.close(index);
+      }
+    );
+  });
+});
+
+function getUserInof() {
+  $.ajax({
+    url: "/my/userinfo",
+    // headers: {
+    //   Authorization: localStorage.getItem("token") || "",
+    // },
+    success: (res) => {
+      //   console.log(res);
+      if (res.status != 0) {
+        return layui.layer.msg(res.message);
+      }
+      renderAvatar(res.data);
+    },
+    // complete: function (res) {
+    //   console.log(res.responseJSON);
+    //   // 判断  如果状态码是1  错误信息是身份认证失败，那么就跳转到登录页面
+    //   let obj = res.responseJSON;
+    //   if (obj.status == 1 && obj.message === "身份认证失败!") {
+    //     // 清空本地token
+    //     localStorage.removeItem("token");
+    //     // 页面跳转
+    //     location.href = "./login.html";
+    //   }
+    // },
+  });
+}
+
+function renderAvatar(user) {
+  let name = user.nickname || user.username;
+  $("#welcome").html("欢迎&nbsp;&nbsp;" + name);
+  if (user.user_pic !== null) {
+    $(".layui-nav-img").show().attr("src", user.user_pic);
+    $("text-avatar").hide();
+  } else {
+    $(".layui-nav-img").hide();
+    let text = name[0].toUpperCase();
+    $(".text-avatar").show().html(text);
+  }
+}
